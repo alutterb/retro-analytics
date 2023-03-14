076ef276237e17fcd9353aef3207fd2bd2ecc20e
Austin <austin@utiliz.app>
2023-03-14 11:19:07 -0500

    working prediction - one example

---
+++ src/modeling/lstm.py
@@ -0,0 +1,91 @@
+from pandas import DataFrame
+from pandas import Series
+from pandas import concat
+from pandas import read_csv
+from pandas import datetime
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from math import sqrt
+from matplotlib import pyplot
+import numpy
+import pickle
+import os
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+data_input_path = os.path.join(script_dir, '../../data/preprocessed/model_input/preprocessed_input.pickle')
+
+
+# inverse scaling for a forecasted value
+def invert_scale(scaler, X, value):
+	new_row = [x for x in X] + [value]
+	array = numpy.array(new_row)
+	array = array.reshape(1, len(array))
+	inverted = scaler.inverse_transform(array)
+	return inverted[0, -1]
+
+# invert differenced value
+def inverse_difference(history, yhat, interval=1):
+	return yhat + history[-interval]
+
+# fit an LSTM network to training data
+def fit_lstm(train, batch_size, nb_epoch, neurons):
+	X, y = train[:, 0:-1], train[:, -1]
+	X = X.reshape(X.shape[0], 1, X.shape[1])
+	model = Sequential()
+	model.add(LSTM(neurons, batch_input_shape=(batch_size, X.shape[1], X.shape[2]), stateful=True))
+	model.add(Dense(1))
+	model.compile(loss='mean_squared_error', optimizer='adam')
+	for i in range(nb_epoch):
+		model.fit(X, y, epochs=1, batch_size=batch_size, verbose=0, shuffle=False)
+		model.reset_states()
+	return model
+
+# make a one-step forecast
+def forecast_lstm(model, batch_size, X):
+	X = X.reshape(1, 1, len(X))
+	yhat = model.predict(X, batch_size=batch_size)
+	return yhat[0,0]
+
+def main():
+	# load data object from file
+	with open(data_input_path, 'rb') as f:
+		data = pickle.load(f)
+	
+	# TEST - retrieve train scaled of one item
+	train_scaled = data[('xbox-one', 'xbox-one-x-1tb-console-project-scorpio-edition')][0]
+	test_scaled = data[('xbox-one', 'xbox-one-x-1tb-console-project-scorpio-edition')][1]
+	lstm_model = fit_lstm(train_scaled, 1, 1000, 4)
+	train_reshaped = train_scaled[:, 0].reshape(len(train_scaled), 1, 1)
+	lstm_model.predict(train_reshaped, batch_size=1)
+
+	predictions = list()
+	for i in range(len(test_scaled)):
+		# make one-step forecast
+		X, y = test_scaled[i, 0:-1], test_scaled[i, -1]
+		yhat = forecast_lstm(lstm_model, 1, X)
+		'''
+		# invert scaling
+		yhat = invert_scale(scaler, X, yhat)
+		# invert differencing
+		yhat = inverse_difference(raw_values, yhat, len(test_scaled)+1-i)
+		'''
+		# store forecast
+		predictions.append(yhat)
+		#expected = raw_values[len(train) + i + 1]
+		#print('Month=%d, Predicted=%f, Expected=%f' % (i+1, yhat, expected))
+
+	print(predictions)
+	return 0
+	# report performance
+	rmse = sqrt(mean_squared_error(raw_values[-12:], predictions))
+	print('Test RMSE: %.3f' % rmse)
+	# line plot of observed vs predicted
+	pyplot.plot(raw_values[-12:])
+	pyplot.plot(predictions)
+	pyplot.show()
+
+if __name__ == "__main__":
+	main()
