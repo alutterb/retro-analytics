3f608a86dbb5160010c7aed51bea676ed14b0964
Austin <austin@utiliz.app>
2023-03-17 14:12:41 -0500

    initial commit of ARIMA model

---
+++ src/data_preprocessing/ARIMA_preprocessing.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import os
+import pickle
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+historical_prices_path = os.path.join(script_dir, '../../data/preprocessed/scraped_data/historical prices.csv')
+data_output_path = os.path.join(script_dir, '../../data/preprocessed/model_input/preprocessed_input_ARIMA.pickle')
+
+
+def clean_timeseries(timeseries):
+    if len(timeseries.strip()) == 0:
+        raise ValueError("Input timeseries string is empty")
+
+    if "," not in timeseries:
+        raise ValueError("Input timeseries string is not properly formatted")
+
+    timeseries = timeseries.strip()
+    if 'nan' in timeseries:
+        return False
+    timeseries = eval(timeseries)
+    return timeseries
+
+
+def check_threshold(arr, threshold):
+    if arr:
+        unique_vals = set(arr)
+        percentage = len(unique_vals) / len(arr)
+        return percentage >= threshold
+    else:
+        return None
+
+
+def main():
+    hist_prices_df = pd.read_csv(historical_prices_path)
+    hist_prices_df = hist_prices_df.drop('Unnamed: 0', axis=1)
+
+    hist_prices_df['historical prices'] = hist_prices_df['historical prices'].apply(clean_timeseries)
+
+    raw_values = hist_prices_df['historical prices']
+    names = list(zip(hist_prices_df['console'], hist_prices_df['game']))
+
+    data = {}
+    for name, timeseries in zip(names, raw_values):
+        if timeseries:
+            if check_threshold(timeseries, 0.4):
+                # In the case of ARIMA, we don't need to make series stationary or scale the data.
+                # We'll store the original timeseries directly.
+                data[name] = timeseries
+            else:
+                data[name] = None
+        else:
+            data[name] = None
+
+    with open(data_output_path, 'wb') as f:
+        pickle.dump(data, f)
+
+
+if __name__ == "__main__":
+    main()
--- src/modeling/training.py
+++ src/modeling/training.py
@@ -1,103 +1,76 @@
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
-from keras.layers import Dropout
-from keras.callbacks import EarlyStopping, LearningRateScheduler
-
-import pickle
 import os
-import math
+import pickle
+import warnings
 
-from multiprocessing import Pool, cpu_count
+import numpy as np
+import pandas as pd
+from statsmodels.tsa.arima.model import ARIMA
+from multiprocessing import Pool
+from sklearn.metrics import mean_squared_error
+
+import itertools
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
-data_input_path = os.path.join(script_dir, '../../data/preprocessed/model_input/preprocessed_input.pickle')
-model_output_path = os.path.join(script_dir, '../../models/LSTM/models.pickle')
+data_input_path = os.path.join(script_dir, '../../data/preprocessed/model_input/preprocessed_input_ARIMA.pickle')
+model_output_path = os.path.join(script_dir, '../../models/ARIMA/models_ARIMA.pickle')
 
+warnings.filterwarnings("ignore")  # Ignore ARIMA warnings for better readability
 
-# Define a learning rate scheduler function for the model training
-def step_decay(epoch):
-	initial_lrate = 0.001  # Initial learning rate
-	drop = 0.5  # Factor by which to decrease the learning rate
-	epochs_drop = 50.0  # How often to decrease the learning rate (every 50 epochs)
-	lrate = initial_lrate * math.pow(drop, math.floor((1+epoch)/epochs_drop))
-	return lrate
+def find_best_order(timeseries, max_p=3, max_d=2, max_q=3):
+    best_order = None
+    best_aic = float("inf")
 
-# fit an LSTM network to training data
-from keras.optimizers import SGD
+    for p, d, q in itertools.product(range(max_p + 1), range(max_d + 1), range(max_q + 1)):
+        if p == 0 and d == 0 and q == 0:
+            continue
+        try:
+            model = ARIMA(timeseries, order=(p, d, q))
+            model_fit = model.fit()
+            if model_fit.aic < best_aic:
+                best_aic = model_fit.aic
+                best_order = (p, d, q)
+        except:
+            continue
+    return best_order
 
-def fit_lstm(train, batch_size, nb_epoch, neurons):
-	X, y = train[:, 0:-1], train[:, -1]
-	X = X.reshape(X.shape[0], 1, X.shape[1])
-	model = Sequential()
-	model.add(LSTM(neurons, return_sequences=True, input_shape=(1, X.shape[2])))
-	model.add(Dropout(0.2))
-	model.add(LSTM(neurons, return_sequences=True))
-	model.add(Dropout(0.2))
-	model.add(LSTM(neurons))
-	model.add(Dropout(0.2))
-	model.add(Dense(1))
-	model.compile(loss='mean_squared_error', optimizer='adam')
-	# Define early stopping to stop training if the validation loss doesn't improve for a certain number of epochs
-	early_stopping = EarlyStopping(monitor='val_loss', patience=20, restore_best_weights=True)
-	
-	# Create a learning rate scheduler using the step_decay function
-	lrate = LearningRateScheduler(step_decay)
-	
-	# Define a list of callbacks to use during training
-	callbacks_list = [early_stopping, lrate]
+def train_arima(key_data_tuple):
+    key, timeseries = key_data_tuple
+    print(f"Training ARIMA model for {key[1]} on {key[0]}")
 
- 	# Fit the model using the training data, validation split, and the defined callbacks
-	model.fit(X, y, validation_split=0.2, epochs=nb_epoch, batch_size=batch_size, verbose=0, shuffle=False, callbacks=callbacks_list)
-	
-	# Reset the model states
-	model.reset_states()
-
-	return model
-
-
-
-def load_data(input_path):
-	with open(input_path, 'rb') as f:
-		data = pickle.load(f)
-	return data
-
-def train_model(key_data_tuple):
-	key, data = key_data_tuple
-	print(f"Training LSTM model for {key[1]} on {key[0]}")
-
-	if data:
-		train_scaled = data[1]
-	else:
-		return key, None
-
-	lstm_model = fit_lstm(train=train_scaled, batch_size=1, nb_epoch=20, neurons=2)
-	return key, lstm_model
+    if timeseries:
+        best_order = find_best_order(timeseries)
+        if best_order is not None:
+            model = ARIMA(timeseries, order=best_order)
+            model_fit = model.fit()
+            return key, model_fit
+        else:
+            return key, None
+    else:
+        return key, None
 
 def train_models(data, num_processes):
-	models = {}
-	if num_processes > 1:
-		with Pool(num_processes) as p:
-			results = p.map(train_model, data.items())
-	else:
-		# Sequential execution
-		results = [train_model(item) for item in data.items()]
+    models = {}
+    with Pool(num_processes) as p:
+        results = p.map(train_arima, data.items())
 
-	for key, model in results:
-		if model is not None:
-			models[key] = model
+    for key, model_fit in results:
+        if model_fit is not None:
+            models[key] = model_fit
+    return models
 
-	return models
-
+def load_data(input_path):
+    with open(input_path, 'rb') as f:
+        data = pickle.load(f)
+    return data
 
 def save_models(models, output_path):
-	with open(output_path, 'wb') as f:
-		pickle.dump(models, f)
+    with open(output_path, 'wb') as f:
+        pickle.dump(models, f)
 
 def main():
-	data = load_data(data_input_path)
-	models = train_models(data, num_processes=1)
-	save_models(models, model_output_path)
-	
+    data = load_data(data_input_path)
+    models = train_models(data, num_processes=1)
+    save_models(models, model_output_path)
+
 if __name__ == "__main__":
-	main()
+    main()
