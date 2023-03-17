c991c632b17682d8335a80e7fae2f12bbfc87ae0
Austin <austin@utiliz.app>
2023-03-16 23:09:27 -0500

    lstm model final pass

--- src/modeling/training.py
+++ src/modeling/training.py
@@ -3,7 +3,6 @@ from keras.layers import Dense
 from keras.layers import LSTM
 from keras.layers import Dropout
 from keras.callbacks import EarlyStopping, LearningRateScheduler
-from keras.optimizers import SGD
 
 import pickle
 import os
@@ -25,6 +24,8 @@ def step_decay(epoch):
 	return lrate
 
 # fit an LSTM network to training data
+from keras.optimizers import SGD
+
 def fit_lstm(train, batch_size, nb_epoch, neurons):
 	X, y = train[:, 0:-1], train[:, -1]
 	X = X.reshape(X.shape[0], 1, X.shape[1])
@@ -37,7 +38,6 @@ def fit_lstm(train, batch_size, nb_epoch, neurons):
 	model.add(Dropout(0.2))
 	model.add(Dense(1))
 	model.compile(loss='mean_squared_error', optimizer='adam')
-
 	# Define early stopping to stop training if the validation loss doesn't improve for a certain number of epochs
 	early_stopping = EarlyStopping(monitor='val_loss', patience=20, restore_best_weights=True)
 	
@@ -55,47 +55,49 @@ def fit_lstm(train, batch_size, nb_epoch, neurons):
 
 	return model
 
+
+
 def load_data(input_path):
-    with open(input_path, 'rb') as f:
-        data = pickle.load(f)
-    return data
+	with open(input_path, 'rb') as f:
+		data = pickle.load(f)
+	return data
 
 def train_model(key_data_tuple):
-    key, data = key_data_tuple
-    print(f"Training LSTM model for {key[1]} on {key[0]}")
+	key, data = key_data_tuple
+	print(f"Training LSTM model for {key[1]} on {key[0]}")
 
-    if data:
-        train_scaled = data[1]
-    else:
-        return key, None
+	if data:
+		train_scaled = data[1]
+	else:
+		return key, None
 
-    lstm_model = fit_lstm(train_scaled, 1, 100, 4)
-    return key, lstm_model
+	lstm_model = fit_lstm(train=train_scaled, batch_size=1, nb_epoch=20, neurons=2)
+	return key, lstm_model
 
 def train_models(data, num_processes):
-    models = {}
-    if num_processes > 1:
-        with Pool(num_processes) as p:
-            results = p.map(train_model, data.items())
-    else:
-        # Sequential execution
-        results = [train_model(item) for item in data.items()]
+	models = {}
+	if num_processes > 1:
+		with Pool(num_processes) as p:
+			results = p.map(train_model, data.items())
+	else:
+		# Sequential execution
+		results = [train_model(item) for item in data.items()]
 
-    for key, model in results:
-        if model is not None:
-            models[key] = model
+	for key, model in results:
+		if model is not None:
+			models[key] = model
 
-    return models
+	return models
 
 
 def save_models(models, output_path):
-    with open(output_path, 'wb') as f:
-        pickle.dump(models, f)
+	with open(output_path, 'wb') as f:
+		pickle.dump(models, f)
 
 def main():
-    data = load_data(data_input_path)
-    models = train_models(data, num_processes=1)
-    save_models(models, model_output_path)
+	data = load_data(data_input_path)
+	models = train_models(data, num_processes=1)
+	save_models(models, model_output_path)
 	
 if __name__ == "__main__":
 	main()
