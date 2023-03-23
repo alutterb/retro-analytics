e2759a84987f99cbd204b764652907ce045267d8
Austin <austin@utiliz.app>
2023-03-23 17:55:41 -0500

    working interface using tkinter

--- src/deployment/results_app.py
+++ src/deployment/results_app.py
@@ -1,55 +1,53 @@
 import tkinter as tk
+from ttkthemes import ThemedTk
 import pandas as pd
-from tkinter import ttk
 
 class ResultsApp(tk.Tk):
-    def __init__(self):
+    def __init__(self, data):
         super().__init__()
+        self.data = data
+        self.filtered_data = data.copy()
+        self.console_var = tk.StringVar(self)
+        self.console_var.set("All Consoles")
 
-        self.title("Game Results")
-        self.geometry("1200x600")
+        self.title("Game Recommendations")
+        self.geometry("800x600")
 
         self.create_widgets()
-        self.load_data()
 
     def create_widgets(self):
-        self.tree = ttk.Treeview(self, columns=("Console", "Game", "Comments Metric", "Posts Metric", "Slope", "Percent Change", "Combined Metric"), show="headings")
-        self.tree.heading("Console", text="Console")
-        self.tree.heading("Game", text="Game")
-        self.tree.heading("Comments Metric", text="Comments Metric")
-        self.tree.heading("Posts Metric", text="Posts Metric")
-        self.tree.heading("Slope", text="Slope")
-        self.tree.heading("Percent Change", text="Percent Change")
-        self.tree.heading("Combined Metric", text="Combined Metric")
+        console_label = tk.Label(self, text="Console:")
+        console_label.pack(side="top")
 
-        self.tree.column("Console", width=120)
-        self.tree.column("Game", width=300)
-        self.tree.column("Comments Metric", width=100)
-        self.tree.column("Posts Metric", width=100)
-        self.tree.column("Slope", width=100)
-        self.tree.column("Percent Change", width=100)
-        self.tree.column("Combined Metric", width=120)
+        consoles = sorted(list(set(self.data["console"])))
+        consoles.insert(0, "All Consoles")
+        console_option_menu = tk.OptionMenu(self, self.console_var, *consoles, command=self.filter_by_console)
+        console_option_menu.pack(side="top")
 
-        self.tree.pack(expand=True, fill=tk.BOTH)
+        self.results_listbox = tk.Listbox(self, width=150, height=20)
+        self.results_listbox.pack(side="top", fill="both", expand=True)
 
-        self.sort_console_button = tk.Button(self, text="Sort by Console", command=self.sort_by_console)
-        self.sort_console_button.pack()
+        self.update_listbox()
 
-    def load_data(self):
-        file_path = "/home/akagi/Documents/Projects/retro-analytics/data/outputs/results.csv" 
-        self.df = pd.read_csv(file_path)
-        self.df.sort_values("combined_metric", ascending=False, inplace=True)
-        self.populate_tree()
+    def update_listbox(self):
+        self.results_listbox.delete(0, tk.END)
+        for _, row in self.filtered_data.iterrows():
+            item = f"{row['console']} - {row['game']} - Combined metric: {row['combined_metric']:.2f}"
+            self.results_listbox.insert(tk.END, item)
 
-    def populate_tree(self):
-        self.tree.delete(*self.tree.get_children())  # Clear the treeview
-        for index, row in self.df.iterrows():
-            self.tree.insert("", tk.END, values=(row["console"], row["game"], row["comments metric"], row["posts metric"], row["slope"], row["percent_change"], row["combined_metric"]))
-
-    def sort_by_console(self):
-        self.df.sort_values(["console", "combined_metric"], ascending=(True, False), inplace=True)
-        self.populate_tree()
+    def filter_by_console(self, console):
+        if console == "All Consoles":
+            self.filtered_data = self.data.copy()
+        else:
+            self.filtered_data = self.data[self.data['console'] == console]
+        self.update_listbox()
 
 if __name__ == "__main__":
-    app = ResultsApp()
+    # Load your data here, for example from a CSV file
+    # data = pd.read_csv('your_data.csv')
+    # In this example, I'll create a sample DataFrame
+    data = pd.read_csv(r'/home/akagi/Documents/Projects/retro-analytics/data/outputs/results.csv')
+
+    root = ThemedTk(theme="arc")
+    app = ResultsApp(data)
     app.mainloop()
