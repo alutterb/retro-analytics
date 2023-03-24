1716c139f73f13331ce7ba695fb6df4e26825f27
Austin <austin@utiliz.app>
2023-03-23 22:51:40 -0500

    included search function

--- src/deployment/results_app.py
+++ src/deployment/results_app.py
@@ -24,6 +24,15 @@ class ResultsApp(tk.Tk):
         console_option_menu = tk.OptionMenu(self, self.console_var, *consoles, command=self.filter_by_console)
         console_option_menu.pack(side="top")
 
+        search_label = tk.Label(self, text="Search:")
+        search_label.pack(side="top")
+        
+        self.search_entry = tk.Entry(self)
+        self.search_entry.pack(side="top")
+
+        search_button = tk.Button(self, text="Search", command=self.search_game)
+        search_button.pack(side="top")
+
         self.results_listbox = tk.Listbox(self, width=150, height=20)
         self.results_listbox.pack(side="top", fill="both", expand=True)
 
@@ -42,10 +51,16 @@ class ResultsApp(tk.Tk):
             self.filtered_data = self.data[self.data['console'] == console]
         self.update_listbox()
 
+    def search_game(self):
+        search_term = self.search_entry.get().lower()
+        if search_term:
+            self.filtered_data = self.data[self.data['game'].str.lower().str.contains(search_term)]
+            self.update_listbox()
+        else:
+            self.filtered_data = self.data.copy()
+            self.update_listbox()
+
 if __name__ == "__main__":
-    # Load your data here, for example from a CSV file
-    # data = pd.read_csv('your_data.csv')
-    # In this example, I'll create a sample DataFrame
     data = pd.read_csv(r'/home/akagi/Documents/Projects/retro-analytics/data/outputs/results.csv')
 
     root = ThemedTk(theme="arc")
