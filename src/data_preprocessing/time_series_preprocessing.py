5b5153a3b4a5af1e337260e7417d146b6ae550fc
Austin <austin@utiliz.app>
2023-03-25 08:31:11 -0500

    workign daily_update.py file

--- src/data_preprocessing/time_series_preprocessing.py
+++ src/data_preprocessing/time_series_preprocessing.py
@@ -8,6 +8,7 @@ output_path = os.path.join(script_dir, '../../data/preprocessed/scraped_data/his
 
 def main():
     prices_df = pd.read_csv(prices_path)
+    prices_df = prices_df.drop_duplicates()  # Remove exact duplicate rows
     grouped_df = prices_df.groupby(['console', 'game'])['price', 'date'].agg({'price': list, 'date': list}).reset_index()
     grouped_df = grouped_df.rename(columns={'price': 'historical prices', 'date': 'historical dates'})
     grouped_df.to_csv(output_path)
